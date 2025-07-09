cbb8e0c3137e564044b26c6e0a6510baf0a7802b
Vanessa Brito <nnessa.rbrito003@gmail.com>
2025-07-09 19:52:58 -0300

    Add files via upload

---
+++ ex28.py
@@ -0,0 +1,27 @@
+#28. Escreva um algoritmo que leia três valores inteiros e diferentes e mostre-os em ordem crescente
+
+a = int(input("Digite o primeiro número inteiro: "))
+b = int(input("Digite o segundo número inteiro: "))
+c = int(input("Digite o terceiro número inteiro: "))
+
+if a == b or a == c or b == c:
+    print("Erro: os três números devem ser diferentes.")
+ 
+else:
+    if a < b and a < c:
+        if b < c:
+            print("Ordem crescente:", a, b, c)
+        else:
+            print("Ordem crescente:", a, c, b)
+    elif b < a and b < c:
+        if a < c:
+            print("Ordem crescente:", b, a, c)
+        else:
+            print("Ordem crescente:", b, c, a)
+
+    else:
+        if a < b:
+            print("Ordem crescente:", c, a, b)
+        else:
+            print("Ordem crescente:", c, b, a)
+
